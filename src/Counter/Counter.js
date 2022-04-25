d0995cb8d0342979baafab8c1b3bd3b61033a40c
billy <billyh880217@gmail.com>
2022-04-25 22:53:57 +1200

    add simple  TTD testing

---
+++ src/Counter/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+	const [counterValue, setCountValue] = useState(0);
+	const [inputValue, setInputValue] = useState(1);
+	return (
+		<div>
+			<h1 data-testid="header">My Counter</h1>
+			<h2 data-testid="counter">{counterValue}</h2>
+			<button data-testid="subtract-btn">-</button>
+			<input data-testid="input" type="number" value={inputValue}/>
+			<button data-testid="add-btn">+</button>
+		</div>
+	);
+}
+
+export default Counter;
---
+++ src/Counter/__test__/Counter.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Counter from "../Counter";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { text } from "cheerio/lib/static";
+
+test("header renders with correct text", () => {
+	const { getByTestId } = render(<Counter />);
+	const headerEl = getByTestId("header");
+
+	expect(headerEl.textContent).toBe("My Counter");
+})
+
+test("Counter initally start with text of 0", () => {
+	const { getByTestId } = render(<Counter />);
+	const counterEl = getByTestId("counter");
+
+	expect(counterEl.textContent).toBe("0");
+
+})
+
+test("input contains inital value is 1", () => {
+	const { getByTestId } = render(<Counter />);
+	const inputEl = getByTestId("input");
+
+	expect(inputEl.value).toBe("1");
+});
+
+test("add button renders with +", () => {
+	const { getByTestId } = render(<Counter />);
+	const addBtn = getByTestId("add-btn");
+
+	expect(addBtn.textContent).toBe("+");
+});
+
+test("subtract button renders with -", () => {
+	const { getByTestId } = render(<Counter />);
+	const subtractBtn = getByTestId("subtract-btn");
+
+	expect(subtractBtn.textContent).toBe("-");
+});
